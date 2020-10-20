6012cac22dcc9a84fe8cdb86426dbf8a5270dfd1
suzukiyuichi1228 <zm17397@josai.ac.jp>
2020-10-20 12:39:21 +0000

    1020

---
+++ ex6-5.js
@@ -0,0 +1,18 @@
+function sayhello(){
+
+    var who = 'world';
+
+    alert('Hello,' + who + '!');
+}
+
+function taro(){
+
+    who = '太郎';
+    alert(who + 'が来た!')
+}
+
+function hanako(){
+
+    who = '花子';
+    alert(who + 'が来た!')
+}
