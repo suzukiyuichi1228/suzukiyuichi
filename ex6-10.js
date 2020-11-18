35279d6d63966f807159b59f8649be94f24cf880
suzukiyuichi1228 <zm17397@josai.ac.jp>
2020-11-18 05:31:24 +0000

    1118

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,16 @@
-function imgchange1(){
-    document.getElementById('logo').src =
-    './image/bulbon.gif';
-}
+function hyoji(){
+    var str = "一文字ずつ表示します。";
+    var cnt = document.timer.moji.value.length;
+
+    if(cnt < 11){
+        document.timer.moji.value=str.substr(0,cnt+1); }
+
+    else{
+        document.timer.moji.value="";}}
+
+    function startfnc(){
+        setInterval("hyoji()",1000);}
+    
+    function mes(){
+        alert("3秒経ちました!");
+    }
---
+++ ex6-12.js
@@ -0,0 +1,7 @@
+function ThreeSecChange(){
+   setInterval("imgchange12()",3000);}
+
+function imgchange12(){
+    document.getElementById('logo2').src = './image/bulbon.gif';
+}
+
